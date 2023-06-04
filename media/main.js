bd7c251c8814a8e3854c1de9572ef60fb9131cfd
Jonathan Pyers <pyersjonathan@gmail.com>
2023-06-03 22:17:25 -0400

    add help panel

--- media/main.js
+++ media/main.js
@@ -22,27 +22,29 @@
     $(node).toggleClass("collapsed", collapsed);
     const arrow = $(node).find(".collapse-arrow");
     arrow.toggleClass("rotate-90", collapsed);
-    vscode.postMessage({type: "collapseCategory", value: {category, collapsed}});
+    vscode.postMessage({ type: "collapseCategory", value: { category, collapsed } });
     console.log("toggleCollapsed", category, collapsed);
   }
-  
-  document.querySelectorAll("[data-collapse-category]").forEach(node => {
+
+  document.querySelectorAll("[data-collapse-category]").forEach((node) => {
     const category = node.dataset.collapseCategory;
     let collapsed = node.dataset.collapsed === "true";
-    
+
     function handleClick() {
       toggleCollapse(node, category, collapsed);
       collapsed = !collapsed;
     }
-    
+
     node.setAttribute("aria-expanded", !collapsed);
     node.setAttribute("aria-hidden", collapsed);
     $(node).siblings(".command-list").toggleClass("hidden", collapsed);
     $(node).toggleClass("collapsed", collapsed);
     node.addEventListener("click", handleClick);
-  
+
     $(node).find(".collapse-arrow").toggleClass("rotate-90", collapsed);
   });
 
-
+  $("#command-help").on("click", () => {
+    $("#help-panel").toggleClass("hidden");
+  });
 })();

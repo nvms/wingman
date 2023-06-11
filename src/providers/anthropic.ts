ee0f5270b6b9bdfc90906ea96b037f2cee6e6f21
capdevc <github@defvar.org>
2023-06-11 00:03:55 -0400

    Vendor the anthropic SDK

--- src/providers/anthropic.ts
+++ src/providers/anthropic.ts
@@ -1,10 +1,15 @@
 /* eslint-disable unused-imports/no-unused-vars */
-import { Client, type CompletionResponse, type SamplingParameters, AI_PROMPT, HUMAN_PROMPT } from "@anthropic-ai/sdk";
 import * as vscode from "vscode";
 
-import { type PostableViewProvider, type Provider } from ".";
+import { type PostableViewProvider, type ProviderResponse, type Provider } from ".";
+import { Client, type CompletionResponse, type SamplingParameters, AI_PROMPT, HUMAN_PROMPT } from "./sdks/anthropic";
 import { type Command } from "../templates/render";
+import { handleResponseCallbackType } from "../templates/runner";
 import { displayWarning, getConfig, getSelectionInfo } from "../utils";
+
+let lastMessage: string | undefined;
+let lastTemplate: Command | undefined;
+let lastSystemMessage: string | undefined;
 
 export class AnthropicProvider implements Provider {
   viewProvider: PostableViewProvider | undefined;
@@ -33,24 +38,49 @@ export class AnthropicProvider implements Provider {
     this._abort = new AbortController();
   }
 
-  async send(message: string, systemMessage?: string, template?: Command): Promise<void> {
-    const isFollowup = typeof this.conversationTextHistory === "string" && this.conversationTextHistory.length > 0;
+  async send(message: string, systemMessage?: string, template?: Command): Promise<void | ProviderResponse> {
+    let isFollowup = false;
 
-    if (!isFollowup) {
-      this.viewProvider?.postMessage({ type: "newChat" });
+    lastMessage = message;
+
+    if (template) {
+      lastTemplate = template;
     }
 
-    // prepend conversation history to message if it's a follow up
-    const newUserMessage = `${this.conversationTextHistory ? `${this.conversationTextHistory}\n\n` : ""}${HUMAN_PROMPT} ${message}`;
-    // prepend the system message if it exists
-    const newPrompt = `${systemMessage ? `${systemMessage}\n\n` : ""}${newUserMessage}`;
+    if (!template && !lastTemplate) {
+      return;
+    }
+
+    if (!template) {
+      template = lastTemplate!;
+      isFollowup = true;
+    }
+
+    if (systemMessage) {
+      lastSystemMessage = systemMessage;
+    }
+    if (!systemMessage && !lastSystemMessage) {
+      return;
+    }
+    if (!systemMessage) {
+      systemMessage = lastSystemMessage!;
+    }
+
+    let prompt;
+    if (!isFollowup) {
+      this.viewProvider?.postMessage({ type: "newChat" });
+      // The first message should have the system message prepended
+      prompt = `${systemMessage ?? ""}${HUMAN_PROMPT} ${message}${AI_PROMPT}`;
+    } else {
+      // followups should have the conversation history prepended
+      prompt = `${this.conversationTextHistory ?? ""}${HUMAN_PROMPT} ${message}${AI_PROMPT}`;
+    }
 
     const samplingParameters: SamplingParameters = {
-      prompt: newPrompt,
+      prompt,
       temperature: template?.temperature ?? (getConfig("anthropic.temperature") as number),
       max_tokens_to_sample: template?.maxTokens ?? (getConfig("anthropic.maxTokens") as number) ?? 4096,
-      stop_sequences: [HUMAN_PROMPT],
-      top_k: template?.numberOfChoices,
+      top_k: template?.numberOfChoices ?? -1,
       model: template?.model ?? (getConfig("anthropic.model") as string) ?? "claude-instant-v1",
     };
 
@@ -60,23 +90,46 @@ export class AnthropicProvider implements Provider {
       const editor = vscode.window.activeTextEditor!;
       const selection = getSelectionInfo(editor);
 
-      console.log(typeof this.instance!.completeStream);
-      const response: CompletionResponse = await this.instance!.completeStream(samplingParameters, {
+      const anthropicResponse: CompletionResponse = await this.instance!.completeStream(samplingParameters, {
         onOpen: (response) => {
           console.log("Opened stream, HTTP status code", response.status);
         },
-        onUpdate: (completionResponse: CompletionResponse) => {
-          console.log("inside update");
-          this.viewProvider?.postMessage({ type: "partialResponse", value: { text: completionResponse.completion } });
+        onUpdate: (partialResponse: CompletionResponse) => {
+          this.viewProvider?.postMessage({ type: "partialResponse", value: this.toProviderResponse(partialResponse) });
         },
         signal: this._abort.signal,
       });
+
+      // Reformat the API response into a ProvderResponse
+      const response = this.toProviderResponse(anthropicResponse);
+
       // Append the last response to the conversation history
-      this.conversationTextHistory = `${newUserMessage}${AI_PROMPT} ${response.completion}`;
+      this.conversationTextHistory = `${this.conversationTextHistory ?? ""}${prompt} ${response.text}`;
+      this.viewProvider?.postMessage({ type: "responseFinished", value: response });
+
+      if (!isFollowup) {
+        handleResponseCallbackType(template, editor, selection, response.text);
+      }
     } catch (error) {
       displayWarning(String(error));
     }
   }
 
-  async repeatLast(): Promise<void> {}
+  async repeatLast() {
+    if (!lastMessage || !lastSystemMessage || !lastTemplate) {
+      return;
+    }
+
+    await this.send(lastMessage, lastSystemMessage, lastTemplate);
+  }
+
+  toProviderResponse(response: CompletionResponse) {
+    return {
+      // for some reason, the response completion text always has a space at the beginning.
+      text: response.completion.trimStart(),
+      parentMessageId: "",
+      converastionId: "",
+      id: "",
+    };
+  }
 }
